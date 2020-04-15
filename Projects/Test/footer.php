c62872a178852ef0f03d3a82f2a5305b1bde625c
Sanjay Sahu Bandla <54032677+Sanjay-Sahu-Bandla@users.noreply.github.com>
2020-04-15 20:09:52 +0530

    Initial commit 2

--- Projects/Test/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-	<strong>Copyright &copy; 2020-2021 <a href="http://bhaktividhan.com">bhaktividhan.com</a>.</strong>
-	All rights reserved.
-	<div class="float-right d-none d-sm-inline-block">
-		<!-- <b>Version</b> 3.0.4 -->
-	</div>
-</footer>
